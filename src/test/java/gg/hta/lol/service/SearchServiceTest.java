fe89de496c9708cf449464b73e7586574942113d
wm9596 <34181174+wm9596@users.noreply.github.com>
2021-02-03 17:22:24 +0900

    검색 서비스 테스트 코드

---
+++ src/test/java/gg/hta/lol/service/SearchServiceTest.java
@@ -0,0 +1,28 @@
+package gg.hta.lol.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import gg.hta.lol.riotapi.DataRequester;
+import gg.hta.lol.service.match.SearchService;
+import lombok.Setter;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = {"file:src/main/webapp/WEB-INF/spring/root-context.xml","file:src/main/webapp/WEB-INF/spring/security_config.xml"})
+@Setter
+public class SearchServiceTest {
+	
+	@Autowired
+	private SearchService service;
+
+	@Autowired
+	private DataRequester dr;
+	
+	@Test
+	public void getTest() {
+		service.getSummonerInfo("크림몬");
+	}
+}
